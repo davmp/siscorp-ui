85dc52877aca6eefa9949ba9cb2643987a2e9b41
davmp <davcartezini@gmail.com>
2025-05-06 12:07:59 -0300

    feat: update sidebar

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import * as React from "react";
+import React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { VariantProps, cva } from "class-variance-authority";
 import { PanelLeft } from "lucide-react";
 
-import { useIsMobile } from "@/hooks";
+import { useIsMobile } from "@/hooks/use-mobile";
 import { cn } from "@/utils";
 import { Button } from "../button";
 import { Input } from "../input";
